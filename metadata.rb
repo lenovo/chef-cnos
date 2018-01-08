8e62538ba12a978c61781c5e0dc60107212b97af
arunktele <alankalapall@lenovo.com>
2018-01-08 02:53:59 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,25 +3,9 @@ maintainer       'Lenovo'
 maintainer_email 'cbhagavathip@lenovo.com'
 license          'BSD-3-Clause'
 description      'Implements an recipes for managing network resources on Lenovo switches'
-version          '1.0'
-
-
-recipe "configUpload"
-This receipe uploads configuration to the switches. 
-
-recipe "configDownload"
-This receipe downloads configuration to the switches. The same configuration can be edited/modified and uploaded back using the configUpload recipe
-
-recipe "imgUpload"
-This receipe upload new OS image to the switch.
-
-recipe "vlan"
-This receipe manages the VLAN (create/delete) provisiong on the switch.
-
-recipe "vlan_intf"
-This receipe provides the management of VLAN properties for ethernet and port-channel interfaces.
-                 
-recipe "ip_intf"
-This receipe provides the management of IP interfaces .
-
-
+version          '1.0.0'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/lenovo/chef-cnos'
+issues_url       'https://github.com/lenovo/chef-cnos/issues'
+supports         'cnos'
+chef_version     '>= 12.4'
